b43b16ec0785a908b97921e2f6152f896c1976a3
Thoralf-M <thoralf.mue@gmail.com>
2020-01-07 18:34:51 +0100

    init

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+import { composeAPI } from '@iota/core'
+
+let iota = composeAPI({
+      provider: 'https://community.tanglebay.org'
+})
+
+
+iota.getNodeInfo()
+    .then(info => {
+			nodeinfo = JSON.stringify(info, null, 1)
+			console.log(info)}
+			)
+		.catch(err => console.log(err))
+
+	export let nodeinfo;
+</script>
+
+<style>
+	pre {
+		color: purple;
+	}
+</style>
+
+<pre>Nodeinfo: {nodeinfo}</pre>
